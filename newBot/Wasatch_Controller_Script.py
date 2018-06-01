57f5734d2e1a3c0836a1858996e41f00583c344c
Edwin <edy.yuan@gmail.com>
2018-06-01 13:10:57 -0700

    Modifications made to the script file.

--- newBot/Wasatch_Controller_Script.py
+++ newBot/Wasatch_Controller_Script.py
@@ -24,10 +24,24 @@ microscopeCommand = Wasatch_Serial_Interface_DirectSerial()
 # Note: Center of field is 5, 5 (mm)
 
 # (x, y) in mm, time in seconds (float)
-#GCommand_BleachLine(microscopeCommand, (1.0, 1.0), (1.0, 5.0), 0.5)
+#lineHeight = 5.0 #[mm]
+#lineXPosition = 0.0 #[mm] from center
+#exposure = 1 #[sec] per line
+#GCommand_BleachLine(microscopeCommand, (5.0+lineXPosition, 5.0-lineHeight/2), (5.0+lineXPosition, 5.0+lineHeight/2), exposure)
 
 #(x,y) in mm, args are (module, center, length of mark, seperation b/ outermost pairs, exposure per line in (s), "V" is vertical, "H" is horizontal)
-GCommand_BleachFiducial(microscopeCommand, (5.0, 5.0), 3.0, 0.4, 0.4, "V")
+#GCommand_BleachFiducial(microscopeCommand, (5.0, 5.0), 5.0, 0.1, 5, "V")
+
+
+#This part below creates lines in different expusues times
+lineHeight = 5.0 #[mm]
+exposures = [0.1, 0.2, 0.5, 1, 2, 5] #sec
+for i in range(len(exposures)):
+    lineXPosition = 2.0 + i*0.1 #[mm]
+    exposure = exposures[i] #[sec] per line
+    print('Current exposure :', exposure)
+    GCommand_BleachLine(microscopeCommand, (lineXPosition, 5.0-lineHeight/2), (lineXPosition, 5.0+lineHeight/2), exposure)
+    
 
 #--> Closes connection:
 microscopeCommand.close()
